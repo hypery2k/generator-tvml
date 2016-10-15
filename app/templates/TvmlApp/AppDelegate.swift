16b81bd5ad817865d6ab9844044758d74ca5770b
Martin Reinhardt <contact@martinreinhardt-online.de>
2016-10-15 08:39:14 +0200

    Update to Swift 3 Syntax (#8)

--- app/templates/TvmlApp/AppDelegate.swift
+++ app/templates/TvmlApp/AppDelegate.swift
@@ -16,10 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate, TVApplicationControllerDe
   var appController: TVApplicationController?
 
 
-  static func getAppURL()-> NSURL{
-    var appURL:NSURL;
+  static func getAppURL()-> URL{
+    var appURL:URL;
     #if (arch(i386) || arch(x86_64)) && os(tvOS)
-      appURL = NSURL(string: "http://localhost:8080/dist/scripts/application.js")!
+      appURL = URL(string: "http://localhost:8080/dist/scripts/application.js")!
     #else
       appURL = NSBundle.mainBundle().URLForResource("application", withExtension: "js",subdirectory: "dist/scripts")!
     #endif
@@ -27,21 +27,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate, TVApplicationControllerDe
   }
 
   func registerSettingsBundle(){
-    NSUserDefaults.standardUserDefaults().registerDefaults([
+    UserDefaults.standard.register(defaults: [
       "flynn_couchdb_url" : "http://localhost:6984/"
       ])
   }
 
   func defaultsChanged(){
     //Get the defaults
-    let defaults = NSUserDefaults.standardUserDefaults()
-    let backendURL: String = defaults.stringForKey("flynn_couchdb_url")!
+    let defaults = UserDefaults.standard
+    let backendURL: String = defaults.string(forKey: "flynn_couchdb_url")!
   }
 
   // MARK: Javascript Execution Helper
 
-  func executeRemoteMethod(methodName: String,arguments: Array<AnyObject>?, completion: (Bool) -> Void) {
-    appController?.evaluateInJavaScriptContext({ (context: JSContext) in
+  func executeRemoteMethod(_ methodName: String,arguments: Array<AnyObject>?, completion: @escaping (Bool) -> Void) {
+    appController?.evaluate(inJavaScriptContext: { (context: JSContext) in
       let appObject : JSValue = context.objectForKeyedSubscript("App")
 
       if appObject.hasProperty(methodName) {
@@ -51,8 +51,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate, TVApplicationControllerDe
   }
 
   // MARK: UIApplicationDelegate
-  func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-    self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    self.window = UIWindow(frame: UIScreen.main.bounds)
 
     let appControllerContext = TVApplicationControllerContext()
 
@@ -75,7 +75,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, TVApplicationControllerDe
   }
 
 
-  func applicationWillResignActive(application: UIApplication) {
+  func applicationWillResignActive(_ application: UIApplication) {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and stop playback
     executeRemoteMethod("onWillResignActive",arguments: [], completion: { (success: Bool) in
@@ -83,7 +83,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, TVApplicationControllerDe
     })
   }
 
-  func applicationDidEnterBackground(application: UIApplication) {
+  func applicationDidEnterBackground(_ application: UIApplication) {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     executeRemoteMethod("onDidEnterBackground",arguments: [], completion: { (success: Bool) in
@@ -91,21 +91,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate, TVApplicationControllerDe
     })
   }
 
-  func applicationWillEnterForeground(application: UIApplication) {
+  func applicationWillEnterForeground(_ application: UIApplication) {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     executeRemoteMethod("onWillEnterForeground",arguments: [], completion: { (success: Bool) in
       // ...
     })
   }
 
-  func applicationDidBecomeActive(application: UIApplication) {
+  func applicationDidBecomeActive(_ application: UIApplication) {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     executeRemoteMethod("onDidBecomeActive",arguments: [], completion: { (success: Bool) in
       // ...
     })
   }
 
-  func applicationWillTerminate(application: UIApplication) {
+  func applicationWillTerminate(_ application: UIApplication) {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     executeRemoteMethod("onWillTerminate",arguments: [], completion: { (success: Bool) in
       // ...
@@ -114,30 +114,30 @@ class AppDelegate: UIResponder, UIApplicationDelegate, TVApplicationControllerDe
 
   // MARK: TVApplicationControllerDelegate
 
-  func appController(appController: TVApplicationController, didFinishLaunchingWithOptions options: [String: AnyObject]?) {
-    print("\(__FUNCTION__) invoked with options: \(options)")
+  func appController(_ appController: TVApplicationController, didFinishLaunching options: [String: Any]?) {
+    print("\(#function) invoked with options: \(options)")
     self.registerSettingsBundle()
     self.defaultsChanged()
-    NSNotificationCenter.defaultCenter().addObserver(self,
-                                                     selector: "defaultsChanged",
-                                                     name: NSUserDefaultsDidChangeNotification,
+    NotificationCenter.default.addObserver(self,
+                                                     selector: #selector(AppDelegate.defaultsChanged),
+                                                     name: UserDefaults.didChangeNotification,
                                                      object: nil)
 
   }
 
-  func appController(appController: TVApplicationController, didFailWithError error: NSError) {
-    print("\(__FUNCTION__) invoked with error: \(error)")
+  func appController(_ appController: TVApplicationController, didFail error: Error) {
+    print("\(#function) invoked with error: \(error)")
 
     let title = "Error Launching Application"
     let message = error.localizedDescription
-    let alertController = UIAlertController(title: title, message: message, preferredStyle:.Alert )
+    let alertController = UIAlertController(title: title, message: message, preferredStyle:.alert )
 
-    self.appController?.navigationController.presentViewController(alertController, animated: true, completion: {
+    self.appController?.navigationController.present(alertController, animated: true, completion: {
       // ...
     })
   }
 
-  func appController(appController: TVApplicationController, didStopWithOptions options: [String: AnyObject]?) {
-    print("\(__FUNCTION__) invoked with options: \(options)")
+  func appController(_ appController: TVApplicationController, didStop options: [String: Any]?) {
+    print("\(#function) invoked with options: \(options)")
   }
 }
